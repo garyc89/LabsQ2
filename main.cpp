9e51a046e0f98c5fe44e4ce4eaf44617a92760ca
garyc89 <138969820+garyc89@users.noreply.github.com>
2023-08-10 12:32:13 +0100

    main.cpp
    Q2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+Serial pc(USBTX, USBRX);  
 
+void fibonacci_loop(int n) {
+    int a = 0, b = 1, nextTerm;
+
+    pc.printf("Fibonacci Series using loops: ");
+    for (int i = 1; i <= n; ++i) {
+        pc.printf("%d, ", a);
+        nextTerm = a + b;
+        a = b;
+        b = nextTerm;
     }
+    pc.printf("\r\n");
 }
 
+int main() {
+    pc.baud(9600);  
+    int n = 10;     
+    fibonacci_loop(n);
+    while (1) {
+        // Keep the main loop running
+    }
+}
